a9379e51b05ee51d954644b8c6d0bff4ce47e0c1
Firax_Cobalt <123969490+Firax-Cobalt@users.noreply.github.com>
2024-07-26 14:46:18 +0500

    Добавил новые папки и файлы (блоки).

--- gulpfile.js
+++ gulpfile.js
@@ -1,2 +1,20 @@
-const gulp = require('gulp');
+const gulp = require("gulp");
+const concat = require("gulp-concat-css");
+const plumber = require("gulp-plumber");
+const del = require("del");
+const browserSync = require("browser-sync").create();
 
+function html() {
+  return gulp.src("src/**/*.html").pipe(plumber()).pipe(gulp.dest("dist/"));
+}
+
+function css() {
+  return gulp
+    .src("src/blocks/**/*.css")
+    .pipe(plumber())
+    .pipe(concat("bundle.css"))
+    .pipe(gulp.dest("dist/"));
+}
+
+exports.html = html;
+exports.css = css;
