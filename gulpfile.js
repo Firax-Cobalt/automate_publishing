955029c98b0630a5d0cd0501eec80c33b164dde6
Karim Farkhutdinov <KF.Farkhutdinov@yandex.ru>
2024-07-28 15:52:34 +0500

    Добавил плагин browserSync Для сервера

--- gulpfile.js
+++ gulpfile.js
@@ -4,8 +4,20 @@ const plumber = require("gulp-plumber");
 const del = require("del");
 const browserSync = require("browser-sync").create();
 
+function serve() {
+  browserSync.init({
+    server: {
+      baseDir: "./dist",
+    },
+  });
+}
+
 function html() {
-  return gulp.src("src/**/*.html").pipe(plumber()).pipe(gulp.dest("dist/"));
+  return gulp
+    .src("src/**/*.html")
+    .pipe(plumber())
+    .pipe(gulp.dest("dist/"))
+    .pipe(browserSync.reload({ stream: true }));
 }
 
 function css() {
@@ -13,23 +25,34 @@ function css() {
     .src("src/blocks/**/*.css")
     .pipe(plumber())
     .pipe(concat("bundle.css"))
-    .pipe(gulp.dest("dist/"));
+    .pipe(gulp.dest("dist/"))
+    .pipe(browserSync.reload({ stream: true }));
 }
 
 function images() {
   return gulp
     .src("src/images/**/*.{jpg,png,svg,gif,ico,webp,avif}")
-    .pipe(gulp.dest("dist/images"));
+    .pipe(gulp.dest("dist/images"))
+    .pipe(browserSync.reload({ stream: true }));
 }
 
 function clean() {
   return del("dist");
 }
 
+function watchFiles() {
+  gulp.watch(["src/**/*.html"], html);
+  gulp.watch(["src/blocks/**/*.css"], css);
+  gulp.watch(["src/images/**/*.{jpg,png,svg,gif,ico,webp,avif"], images);
+}
+
 const build = gulp.series(clean, gulp.parallel(html, css, images));
+const watchapp = gulp.parallel(build, watchFiles, serve);
 
 exports.html = html;
 exports.css = css;
 exports.images = images;
 exports.clean = clean;
+
 exports.build = build;
+exports.watchapp = watchapp;
